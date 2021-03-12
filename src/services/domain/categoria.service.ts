9928fcb10f88855990404af8b4f47dd611273ba5
Marcos Melero <mmelero@gmail.com>
2021-03-12 19:45:13 -0300

    Listando categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { CategriaDTO } from "../../models/Categoria.dto";
+import { Observable } from "rxjs/Rx";
+
+@Injectable()
+export class CategoriaService{
+
+    constructor(public http: HttpClient){
+    }
+    findAll() : Observable<CategriaDTO[]> {
+        return this.http.get<CategriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+}
